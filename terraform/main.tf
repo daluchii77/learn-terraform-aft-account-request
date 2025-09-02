ead3200f688fbd407ca3c4cf7ef279a53f3f41cd
Afeez Azeez <azeezafeez119@gmail.com>
2025-09-02 18:40:03 +0100

    account creation using AFT

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,21 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
+    AccountEmail              = "azeezafeez119+Ooshlink1@gmail.com"
+    AccountName               = "Oosh-admin"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    SSOUserEmail              = "<afeezazeez119@gmail.com>"
+    SSOUserFirstName          = "Ooshlink"
+    SSOUserLastName           = "Admin"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "Learn Tutorial" = "AFT Multi Account Setup"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "Ooshlink"
+    change_reason       = "Provision account with AFT"
   }
 
   custom_fields = {
