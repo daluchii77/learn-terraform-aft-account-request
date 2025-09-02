50300e366941fc8d441a6880ac3babcc6d37fdb2
Afeez Azeez <azeezafeez119@gmail.com>
2025-09-02 23:32:10 +0100

    multi account creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,35 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+
+
+module "sandbox1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "azeezafeez119+Ooshlink2@gmail.com"
+    AccountName               = "Oosh-admin2"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "<afeezazeez119@gmail.com>"
+    SSOUserFirstName          = "Ooshlink"
+    SSOUserLastName           = "Admin2"
+  }
+
+
+  account_tags = {
+    # "Learn Tutorial" = "AFT Multi Account Setup"
+     "Purpose" = "AFT Multi Account Setup"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Ooshlink"
+    change_reason       = "Provision account with AFT"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
