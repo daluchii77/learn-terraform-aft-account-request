24ec8aaafbadc67718c43b9b8a648312b7df2c54
Afeez Azeez <azeezafeez119@gmail.com>
2025-09-03 09:18:16 +0100

    recreating admin 1 account that failed to create earlier

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,3 +59,34 @@ module "sandbox1" {
 
   account_customizations_name = "sandbox"
 }
+
+
+module "sandbox2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "azeezafeez119+Ooshlink1@gmail.com"
+    AccountName               = "Oosh-admin1"
+    ManagedOrganizationalUnit = "Production (ou-z0bg-iyb6vpy0)"
+    SSOUserEmail              = "<afeezazeez119@gmail.com>"
+    SSOUserFirstName          = "Ooshlink"
+    SSOUserLastName           = "Admin1"
+  }
+
+
+  account_tags = {
+    # "Learn Tutorial" = "AFT Multi Account Setup"
+     "Purpose" = "AFT Multi Account Setup"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Ooshlink"
+    change_reason       = "Provision account with AFT"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
